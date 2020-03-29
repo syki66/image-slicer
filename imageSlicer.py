b4a297dd2680dd9ebd29138d3a6f42e188bb1a89
syki66 <syeonki66@gmail.com>
2020-03-29 21:36:21 +0900

    height changed

--- imageSlicer.py
+++ imageSlicer.py
@@ -4,20 +4,23 @@ fileName = "suvin.png"
 
 img = Image.open(fileName)
 
+# 위메프는 세로길이 3000px, 옥션은 2580px
+cropped_height = 2580
+
 pic_width, pic_height = img.size
 
 print(pic_width)
 
-last_pic = pic_height % 3000
+last_pic = pic_height % cropped_height
 
 i = 0
 
-while(pic_height / 3000 >i+1):
-    box = (0,(3000*i)+i,pic_width,(3000*i)+i+3000)
+while(pic_height / cropped_height >i+1):
+    box = (0,(cropped_height*i)+i,pic_width,(cropped_height*i)+i+cropped_height)
     img2 = img.crop(box)
     img2.save(fileName.split(".")[0]+str(i+1)+'.'+ fileName.split(".")[1])
     i+=1
 
-box = (0,(3000*i)+i,pic_width,(3000*i)+i+last_pic)
+box = (0,(cropped_height*i)+i,pic_width,(cropped_height*i)+i+last_pic)
 img2 = img.crop(box)
 img2.save(fileName.split(".")[0]+str(i+1)+'.'+fileName.split(".")[1])
